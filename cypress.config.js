69f2cb0742067526ce2b7f3c23dc07fe28322db2
Jeimmy <jeimmycammed@gmail.com>
2022-11-14 00:28:54 +0000

    Correción a Base Url

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:'http://localhost:2372'
+    baseUrl:'http://localhost:2372' 
      
   }
 })
