b340a3e492aec589f8efee0d95a5fea7c35f5850
Pedro Higuera <p.higuera@uniandes.edu.co>
2022-11-27 23:30:25 -0500

    generate random scenes

--- cypress/e2e/GhostTestCases/memberSpec.cy.js
+++ cypress/e2e/GhostTestCases/memberSpec.cy.js
@@ -1,282 +1,192 @@
-const baseUrl = Cypress.env('ghostVersion') || 'new';
-describe('My First Test', () => {
-    beforeEach(()=>{
-      cy.request('POST', '/ghost/api/admin/session', {
-        username: Cypress.env('username'),
-        password: Cypress.env('password')
-      })
+const { faker } = require("@faker-js/faker");
+
+const scenes = [
+  {
+    id: 'VISIT_MEMBER_OPTION',
+    childs: [
+      'CREATE_MEMBER',
+      'GETTING_LAST_MEMBER'
+    ]
+  },
+  {
+    id: 'CREATE_MEMBER',
+    childs: [
+      'RETRY_SAVE_MEMBER'
+    ]
+  },
+  {
+    id: 'DELETE_LAST_MEMBER',
+    childs: [
+      'VALIDATING_REMOVING_MEMBER',
+      'CANCELLING_REMOVING_MEMBER'
+    ]
+  },
+  {
+    id: 'GETTING_LAST_MEMBER',
+    childs: [
+      'DELETE_LAST_MEMBER'
+    ]
+  },
+  {
+    id: 'RETRY_SAVE_MEMBER',
+    childs: [
+      
+    ]
+  },
+  {
+    id: 'VALIDATING_REMOVING_MEMBER',
+    childs: [
+      
+    ]
+  },
+  {
+    id: 'CANCELLING_REMOVING_MEMBER',
+    childs: [
+      
+    ]
+  }
+]
+
+describe('', ()=>{
+  beforeEach(()=>{
+          cy.request('POST', '/ghost/api/admin/session', {
+            username: Cypress.env('username'),
+            password: Cypress.env('password')
+          })
+        })
+  for (let i = 0; i < 15; i++) {
+    const name = faker.name.fullName();
+    const email = faker.internet.email();
+    console.log(name)
+    console.log(email)
+    const deepLevel = getRandomInt(3) + 1
+    it(`Test #${i}A`, ()=> {
+      visitMemberOption()
+      recursiveChilds('VISIT_MEMBER_OPTION', deepLevel, 0, name, email)
     })
-    it('CY11 Try to subscribe with an non-created member', () => {
-      const testId = 'CY11', screenshotId = 1
-      cy.visit('/')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('a.gh-head-button.gh-portal-close').click({ force: true })
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('.gh-portal-signup-message > button').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('#input-email').type('test@test.com')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('button.gh-portal-btn.gh-portal-btn-main.gh-portal-btn-primary').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').should('not.contain', 'Now check your email!')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+  }
+  for (let i = 0; i < 15; i++) {
+    const name = faker.name.fullName();
+    const email = faker.name.fullName();
+    console.log(name)
+    console.log(email)
+    const deepLevel = getRandomInt(3) + 1
+    it(`Test #${i}B`, ()=> {
+      visitMemberOption()
+      recursiveChilds('VISIT_MEMBER_OPTION', deepLevel, 0, name, email)
     })
-  
-    it('CY12 Subscribe with a new member', () => {
-      const testId = 'CY12', screenshotId = 1
-      cy.visit('/ghost/dashboard')
+  }
+})
+
+function recursiveChilds(key, deepLevel, counter, name, email){
+  const event = scenes.find(scene => scene.id == key)
+  console.log(event)
+  if(event.childs.length == 0)
+    return null
+  if (counter == deepLevel)
+    return null
+  const newEvent = gettingRandomEvent(event.childs)
+  switch (newEvent) {
+    case 'VISIT_MEMBER_OPTION':
+      visitMemberOption()
+      break;
+    case 'CREATE_MEMBER':
+      createMember(name, email)
+      break;
+    case 'DELETE_LAST_MEMBER':
+      deleteLastMember()
+      break;
+    case 'GETTING_LAST_MEMBER':
+      gettingLastMember()
+      break;
+    case 'RETRY_SAVE_MEMBER':
+      retrySaveMember(name, email)
+      break;
+    case 'VALIDATING_REMOVING_MEMBER':
+      validatingRemovingMember()
+      break;
+    case 'CANCELLING_REMOVING_MEMBER':
+      cancellingRemovingMmeber()
+      break;  
+    default:
+      break;
+  }
+  counter++;
+  return recursiveChilds(newEvent, deepLevel, counter, name, email)
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function gettingRandomEvent(childs){
+  return childs[getRandomInt(childs.length)]
+}
+
+function visitMemberOption(){
+  cy.visit('/ghost/dashboard')
       cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
       cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+}
+
+function createMember(name, email){
       cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("p.higuera@uniandes.edu.co")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
+      cy.get('#member-name').type(name)
+      
+      cy.get('#member-email').type(email)
+      
       cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-      cy.visit('/')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('a.gh-head-button.gh-portal-close').click({ force: true })
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('.gh-portal-signup-message > button').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('#input-email').type('p.higuera@uniandes.edu.co')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('button.gh-portal-btn.gh-portal-btn-main.gh-portal-btn-primary').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(3000)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').should('not.contain', 'No member exists with this e-mail address. Please sign up first.')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY13 Try to create a member with an used email ', () => {
-      const testId = 'CY13', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("p.higuera@uniandes.edu.co")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+
       cy.on('uncaught:exception', (err, runnable) => {
         return false
       })
+      
       cy.wait(1000)
-      cy.get('p.response').should('contain','Member already exists. Attempting to add member with existing email address')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY14 Try to create a member with a wrong format email ', () => {
-      const testId = 'CY14', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.on('uncaught:exception', (err, runnable) => {
-        return false
-      })
-      cy.wait(1000)
-      cy.get('p.response').should('contain','Invalid Email.')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY15 Subscribe with a new member with the retry option ', () => {
-      const testId = 'CY15', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.on('uncaught:exception', (err, runnable) => {
-        return false
-      })
-      cy.wait(1000)
-      cy.get('#member-email').clear().type("pahf33@gmail.com")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Retry').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-  
-      cy.visit('/')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('a.gh-head-button.gh-portal-close').click({ force: true })
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('.gh-portal-signup-message > button').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('#input-email').type("pahf33@gmail.com")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').find('button.gh-portal-btn.gh-portal-btn-main.gh-portal-btn-primary').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(3000)
-      cy.iframe('#ghost-portal-root > div:first > iframe[title=portal-popup]').should('not.contain', 'No member exists with this e-mail address. Please sign up first.')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY16 Deleting a member', () => {
-      const testId = 'CY16', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('tr > a:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+}
+
+function deleteLastMember(){
       cy.get('.view-actions button:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
       cy.contains('span','Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
       cy.wait(1000)
-      cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
+}
+
+function validatingRemovingMember(){
+  cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Delete member').click()
+      
       cy.wait(1000)
-      cy.get('.epm-modal-container').should('contain','Are you sure you want to leave this page?')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY17 Cancel deleting a member', () => {
-      const testId = 'CY17', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('tr > a:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('.view-actions button:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('span','Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-      cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Cancel').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-      cy.get('.epm-modal-container').should('not.exist')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY18 Leave the page when deleting a member', () => {
-      const testId = 'CY18', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-  
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("pahf33@gmail.com")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-  
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('tr > a:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('.view-actions button:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('span','Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-      cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-      cy.get('.epm-modal-container .modal-footer').contains('button > span','Leave').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+  cy.get('.epm-modal-container .modal-footer').contains('button > span','Leave').click()
+      
       cy.wait(1000)
       cy.url().should('include','/ghost/#/members')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY19 Stay in page when deleting a member', () => {
-      const testId = 'CY19', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-  
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("test")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("pahf33@gmail.com")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-  
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('tr > a:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('.view-actions button:first').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('span','Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
+}
+
+function cancellingRemovingMmeber(){
+
       cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Delete member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      
       cy.wait(1000)
-      cy.get('.epm-modal-container .modal-footer').contains('button > span','Leave').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
+      cy.get('.epm-modal-container .modal-footer').contains('button > span', 'Cancel').click()
+      
+      cy.wait(1000)
       cy.get('.epm-modal-container').should('not.exist')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  
-    it('CY20 List the created member', () => {
-      const testId = 'CY20', screenshotId = 1
-      cy.visit('/ghost/dashboard')
-      cy.wait(1000)
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-  
-      cy.contains('a > span', 'New member').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-name').type("Pedro Higuera")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('#member-email').type("pahf33@gmail.com")
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.contains('button > span', 'Save').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.wait(1000)
-  
-      cy.contains('li','Members').click()
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-      cy.get('.gh-list > tbody > tr:first > a > div > div > h3').should('contain','Pedro Higuera')
-      cy.screenshot(`${baseUrl}/${testId}-${screenshotId++}`)
-    })
-  })
+}
+
+
+function gettingLastMember(){
+  cy.get('tr > a:first').click()
+}
+
+function retrySaveMember(name, email){
+
+  cy.get('#member-email').clear().type("test")
+  cy.get('#member-name').clear().type(name)  
+  cy.contains('button > span', 'Save').click()
+  cy.get('#member-email').clear().type(email)
+  cy.get('#member-name').clear().type(name)  
+  cy.contains('button > span', 'Retry').click()
+}
